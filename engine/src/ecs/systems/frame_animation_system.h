3df39da320983748d40a10c12b150b8852880924
YexionLin <lhl4040294@gmail.com>
2024-05-10 10:07:09 +0800

    change animation system file name

---
+++ engine/src/ecs/systems/frame_animation_system.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ecs/system.h"
+
+namespace fuse::ecs
+{
+    struct frame_animation_system:system
+    {
+       FUSE_INLINE void update()
+    };
+    
+} // namespace fuse::ecs
