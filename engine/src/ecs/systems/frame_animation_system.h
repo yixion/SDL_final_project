eb6e72607902f51017c8db62336de33ca6bf200a
YexionLin <lhl4040294@gmail.com>
2024-05-10 11:16:42 +0800

    complete animation system

--- engine/src/ecs/systems/frame_animation_system.h
+++ engine/src/ecs/systems/frame_animation_system.h
@@ -5,7 +5,30 @@ namespace fuse::ecs
 {
     struct frame_animation_system:system
     {
-       FUSE_INLINE void update()
+       FUSE_INLINE void update(float){
+        for(auto& e:view<animation_component>()){
+            //get components
+            auto& tr = e.get_component<transform_component>();
+            auto& sp = e.get_component<sprite_component>();
+            
+            //get animation from asset registry
+            auto& anim = _assets->get<animation_asset>(an.animation)->instance;
+
+            //compute current frame
+            int index = (get_ticks_ms()/anim.speed) % anim.frames.size();
+            auto& frame = _assets->get<texture_asset>(anim.frames[index])->instance;
+
+            //define target viewport
+            SDL_FRect dst{
+                tr.translate.x, tr.translate.y,
+                sprite.width * tr.scale.x, sprite.height * tr.scale.y
+            };
+
+            //render sprite
+            SDL_RenderCopyExF(_renderer, sprite.data, NULL, &dst, tr.rotation, NULL, sp.flip);
+
+        }
+       }
     };
     
 } // namespace fuse::ecs
